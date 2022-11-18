2d456d4c9d48575f8a03d7520542ce867fcdddd1
mkfoyw <2415384330@qq.com>
2022-11-18 08:26:49 +0000

    update  network config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,41 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv"
+
+dotenv.config()
+
+function mnemonic() {
+  return process.env.PRIVATE_KEY ?? "";
+}
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 100,
+      }
+    }
+  },
+  networks: {
+    xsc_test: {
+      url: process.env.XSC_TEST,
+      accounts: [mnemonic()],
+    },
+    xsc_main: {
+      url: process.env.XSC_MAIN,
+      accounts: [mnemonic()]
+    },
+    eth_rinkeby: {
+      url: process.env.ETH_RINKEBY,
+      accounts: [mnemonic()],
+    },
+    eth_main: {
+      url: process.env.ETH_MAIN,
+      accounts: [mnemonic()]
+    }
+  }
 };
 
 export default config;
